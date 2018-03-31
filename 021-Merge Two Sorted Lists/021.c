1c99a03756c35101a09de377a3412f564ed9b6ab
whansolo <whansolo.tw@gmail.com>
2018-03-31 20:18:08 +0800

    updated: merge two sorted lists

--- 021-Merge Two Sorted Lists/021.c
+++ 021-Merge Two Sorted Lists/021.c
@@ -8,18 +8,8 @@
 
 /*** iterative solution ***/
 struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2) {
-    if(l1 == NULL) return l2;
-    if(l2 == NULL) return l1;
-
-    struct ListNode* head = NULL;
-    if(l1->val < l2->val){
-        head = l1;
-        l1 = l1->next;
-    }
-    else{
-        head = l2;
-        l2 = l2->next;
-    }
+    struct ListNode* head = (struct ListNode *)malloc(sizeof(struct ListNode));
+    head->next = NULL;
 
     struct ListNode* ptr = head;
 
@@ -37,7 +27,7 @@ struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2) {
     if(l1) ptr->next = l1;
     else ptr->next = l2;
 
-    return head;
+    return head->next;
 }
 
 /***  recursive solution  ***/
